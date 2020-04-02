de375e36e9949b49461be771e0745c26e6374c88
Stefan Schneider <stefan.schneider@upb.de>
2020-04-02 16:13:15 +0200

    don't write index column

--- src/algorithms/centralized/bjointsp_algo.py
+++ src/algorithms/centralized/bjointsp_algo.py
@@ -255,25 +255,25 @@ class BJointSPAlgo:
 
 if __name__ == "__main__":
     # for testing and debugging
-    # # Simple test params
-    # network = 'abilene_11.graphml'
-    # args = {
-    #     'network': f'../../../params/networks/{network}',
-    #     'service_functions': '../../../params/services/3sfcs.yaml',
-    #     'config': '../../../params/config/simple_config.yaml',
-    #     'seed': 9999,
-    #     'output_path': f'bjointsp-out/{network}'
-    # }
-
-    # Evaluation params
-    network = 'dfn_58.graphml'
+    # Simple test params
+    network = 'abilene_11.graphml'
     args = {
         'network': f'../../../params/networks/{network}',
         'service_functions': '../../../params/services/3sfcs.yaml',
-        'config': '../../../params/config/hc_0.5.yaml',
+        'config': '../../../params/config/simple_config.yaml',
         'seed': 9999,
         'output_path': f'bjointsp-out/{network}'
     }
+
+    # Evaluation params
+    # network = 'dfn_58.graphml'
+    # args = {
+    #     'network': f'../../../params/networks/{network}',
+    #     'service_functions': '../../../params/services/3sfcs.yaml',
+    #     'config': '../../../params/config/hc_0.5.yaml',
+    #     'seed': 9999,
+    #     'output_path': f'bjointsp-out/{network}'
+    # }
 
     # Setup logging to screen
     logging.basicConfig(level=logging.INFO)
