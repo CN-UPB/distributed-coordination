d0e39147bf1e141c9275326ac8be09ad7c4d709b
Stefan Schneider <stefan.schneider@upb.de>
2020-04-07 13:56:07 +0200

    just llc

--- src/algorithms/execution/3x3/iterator.py
+++ src/algorithms/execution/3x3/iterator.py
@@ -12,7 +12,7 @@ def main():
     pparallel = int(sys.argv[3])
     poll_pause = int(sys.argv[4])
 
-    scenarios = ['llc', 'lnc', 'hc']
+    scenarios = ['llc']
     # networks = ['../../../../params/networks/bics_34.graphml', '../../../../params/networks/dfn_58.graphml',
     #             '../../../../params/networks/intellifiber_73.graphml']
     # ingress = ['0.1', '0.15', '0.2', '0.25', '0.3', '0.35', '0.4', '0.45', '0.5']
