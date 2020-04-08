19daf4b46c6c49f96cb31083f5386d77f7c8f00a
Stefan Schneider <stefan.schneider@upb.de>
2020-04-08 14:21:54 +0200

    renamed eval notebooks

--- src/algorithms/execution/3x3/iterator.py
+++ src/algorithms/execution/3x3/iterator.py
@@ -12,17 +12,17 @@ def main():
     pparallel = int(sys.argv[3])
     poll_pause = int(sys.argv[4])
 
-    scenarios = ['lnc', 'hc']
+    scenarios = ['hc']
     # networks = ['../../../../params/networks/bics_34.graphml', '../../../../params/networks/dfn_58.graphml',
     #             '../../../../params/networks/intellifiber_73.graphml']
     # ingress = ['0.1', '0.15', '0.2', '0.25', '0.3', '0.35', '0.4', '0.45', '0.5']
     # algos = ['gpasp', 'spr1', 'spr2']
     # scenarios = ['hc']
     networks = ['../../../../params/networks/dfn_58.graphml']
-    # ingress = ['0.4']
-    ingress = ['0.1', '0.2', '0.3', '0.4', '0.5']
-    # algos = ['gpasp', 'spr2', 'bjointsp', 'bjointsp_recalc']
-    algos = ['bjointsp_recalc']
+    ingress = ['0.3']
+    # ingress = ['0.1', '0.2', '0.3', '0.4', '0.5']
+    algos = ['gpasp', 'spr2', 'bjointsp', 'bjointsp_recalc']
+    # algos = ['bjointsp_recalc']
 
     running_processes = []
     for r in runs:
